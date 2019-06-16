e33af596a16203686440dacb191efea82a6783f9
mesadhan <sadhansarker3@gmail.com>
2019-06-16 23:36:22 +0600

    updated

--- projects.js
+++ projects.js
@@ -1,5 +1,4 @@
 let projects = [
-
     {
         'title': 'Online Admission System for Daffodil International University',
         'description': 'Applicants are using this system to apply in Daffodil International University on Any subjects and pay their admission fee. Admin offices are used for reporting, verification and SMS purposes.',
@@ -8,14 +7,39 @@ let projects = [
         'tools': 'Angular5, Angular Material, Git, Flex-Layout, Java, Spring Boot, REST APIs etc.'
     },
     {
-        'title': 'Online Admission System for Daffodil International University',
-        'description': 'Applicants are using this system to apply in Daffodil International University on Any subjects and pay their admission fee. Admin offices are used for reporting, verification and SMS purposes.',
+        'title': 'Grade Sheet Approval for Daffodil International University',
+        'description': 'Multiple types of Grade sheet Approval setup manageable for admin offices. Base on given setup admin officer approve submitted grade sheet.',
         'role': 'Associate Frontend & Backend Developer',
-        'demoLinks': ['http://admission.daffodilvarsity.edu.bd'],
+        'demoLinks': ['http://gradesheetapprove.daffodilvarsity.edu.bd'],
         'tools': 'Angular5, Angular Material, Git, Flex-Layout, Java, Spring Boot, REST APIs etc.'
+    },
+    {
+        'title': 'School Management System with Mobile Application',
+        'description': 'This application fully customizable for School management. System Features are Student Admission able, Subject Assignable, Exam Creatable, Automatic ID card generate able, Teachers and Students Attendance Take able through manually or Bio-Metric fingerprint Device. Teacher can assign students Exam marks though this system. Mark sheet will be automatically generatable. System intergraded with bull SMS APIs so when student punch on biometric device it able to send SMS to their guardian. Admin users also generate students repots. This application continuously developing base on the institute requirements.',
+        'role': 'Developer & Designer',
+        'demoLinks': ['http://soft.inktechs.com'],
+        'tools': 'Laravel, Passport, OAuth2, Caching, Notification, MySQL, Composer, and Git, Html, CSS, Bootstrap 3, JavaScript, Angular.js, AJAX, REST and JSON.'
+    },
+    {
+        'title': 'Who Is Caller',
+        'description': 'WhoIsCaller: Caller ID & Call Blocker is the best app for identifying unknown callers and number blocking functionality. It is the simple and most accurate Caller ID app can help you instantly identify the incoming calls not in your contact list.\n' + 'Features are Tracks the name, Operator of incoming number and location of the caller at the time of incoming call, Add numbers to blacklist, Complete information of any mobile number with its Service provider, Easy Call Logs, Easily you can detect received call, missed call, rejected call, Call drop call, Caller Profile with details, Search any Number from our huge contact list, Smart Contact list.',
+        'role': 'Developer and Designer',
+        'demoLinks': ['https://play.google.com/store/apps/details?id=com.pixonlab.whoiscaller'],
+        'tools': 'Laravel, PHP, MySQL, Git, RESTful API, Android, Broadcast receiver, service, SQLite Database, Android Telephony Manager, JSON, Volley, Material Design, Telephony Manager, XML.'
+    },
+    {
+        'title': 'Last Minute News',
+        'description': 'Newspaper application. This application develops focused on Bangladeshi Users but Country base categories available. Features Read the best news from national & regional newspapers & websites, Browse by categories- a personalized home screen with easy access to your favorite topics. Add Categories to your news home, Read, like, and share, share news over Hangout, SMS, WhatsApp, Email, Facebook or any other app you have installed, Keep updated with the latest news from different popular newspapers. Read news offline, Push Notification.',
+        'role': 'Developer',
+        'demoLinks': ['https://play.google.com/store/apps/details?id=news.pixonlab.com'],
+        'tools': 'PHP, Lumen, MySQL, Web Scraping, Git, RESTful API, Android, Java, SQLite Database, JSON, Volley, AdMob Integrations, Services, GSON, Picasso, Material Design, XML.'
     },
 
 ];
+
+
+
+
 
 
 let projectSection = document.getElementById('projectSection');
